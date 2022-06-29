842d32bb81b965a8190b4eefbed25f904bab176f
Tema <tema@Tema.local>
2022-06-29 20:24:27 +0300

    homework lesson 2

---
+++ Web-module/United.php
@@ -0,0 +1,9 @@
+<?php
+
+    $x = $_REQUEST["num1"];
+    $y = $_REQUEST["num2"];
+    $result = $x + $y;
+
+    echo("В жуткой куче шевелится $result душ");
+
+?>
---
+++ Web-module/Unity_of_souls.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>Сошьем все души</title>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <h1>Hello from RAZOR</h1>
+        <form action="United.php">
+            <input name="num1" />
+            <input name="num2" />
+            <button>Сгребу все твои души в одну кучу</button>
+        </form>
+    </body>
+</html>
