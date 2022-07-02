bdbb7095dc40bc650316875d47918f389a9e98e6
Tema <tema@Tema.local>
2022-07-02 16:41:58 +0300

    cookie fix bugs

--- Web-module/click_count.php
+++ Web-module/click_count.php
@@ -13,20 +13,21 @@
         <input type="submit" value="Еще череп для трона" />
     </form>
     <?php
-//        if (isset($_SESSION["count"]))
-//            $i = $_SESSION["count"];
-//        else
-//            $i = -1;
+    //        if (isset($_SESSION["count"]))
+    //            $i = $_SESSION["count"];
+    //        else
+    //            $i = -1;
 
-if (isset($_COOKIE["count"]))
-            $i = $_COOKIE["count"];
-        else
-            $i = -1;
-
-        $i += 1;
-        echo "$i черепов";
-//        $_SESSION["count"] = $i;
-        setcookie("count"), $i);
+    if (isset($_COOKIE["count"])) {
+        $i = $_COOKIE["count"];
+    }
+    else {
+        $i = -1;
+    }
+    $i += 1;
+    echo "$i черепов";
+    //        $_SESSION["count"] = $i;
+    setcookie("count"), $i);
     ?>
 
 </body>
