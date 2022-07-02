5f7fb15f86048f8c94cf0391abdae8027b41960c
Tema <tema@Tema.local>
2022-07-02 14:36:31 +0300

    add login page 9

--- Web-module/check_login.php
+++ Web-module/check_login.php
@@ -21,9 +21,10 @@
 
         $conn = mysqli_connect("localhost:3306", "root", "", "cyb3");
         $result = mysqli_query($conn, $sql);
-        var_dump(mysqli_num_rows($result));
+        //var_dump(mysqli_num_rows($result));
+        $num_rows = mysqli_num_rows($result);
 
-        if ($pwd == "123456") {
+        if ($num_rows == 1) {
         echo "<h1>Тзинч забрал вашу душу, $user</h1>";
         }
         else {
