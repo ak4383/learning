0f2af1fdd0b85afeab3fee9ab6b4872cb69f63bd
Tema <tema@Tema.local>
2022-07-02 12:16:11 +0300

    add login page and minus in calc and web/DC in ignore

---
+++ Web-module/login.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+    <title>Login</title>
+    <meta charset="utf-8">
+    <style>
+        input {
+            width: 100px;
+            margin: 5px;
+        }
+
+    </style>
+
+</head>
+
+<body>
+    <a href = "index1.html">Yankee go home</a> <br />
+    <h1>Для внесения душ нужно сначала отдать свою:</h1>
+
+        <form>
+            <input type="text" name="txtUser" /> <br />
+            <input type="password" name="pwd" /> <br />
+            <input type="submit" value="Возьми мою душу" />
+
+    </form>
+
+</body>
+</html>
