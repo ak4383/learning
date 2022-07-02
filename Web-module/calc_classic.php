3a9adb8d1c7fa8cc063bc9cda77fbca7d30ee165
Tema <tema@Tema.local>
2022-07-02 19:52:59 +0300

    classic php calc add

---
+++ Web-module/calc_classic.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+    <title>Calc</title>
+    <meta charset="utf-8">
+
+    <style>
+        input, button {
+            margin: 6px;
+        }
+
+        button {
+            width: 65px;
+        }
+    </style>
+
+</head>
+
+<body>
+    <a href = "index1.html">Yankee go home</a> <br />
+    <h1>Калькулятор на классическом PHP</h1>
+    <form>
+    <input type="text" name="num1" /> <br />
+    <input type="text" name="num2" /> <br />
+    <button>+</button>
+    <button>-</button> <br />
+    </form>
+    <input type="text"/>
+</body>
+</html>
