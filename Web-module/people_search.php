8285073e44916f14148d04fd27c96a89f25a6c4d
Tema <tema@192.168.1.3>
2022-07-03 20:28:03 +0300

    test json vs xml 7

--- Web-module/people_search.php
+++ Web-module/people_search.php
@@ -3,10 +3,19 @@
 <head>
     <title>Поиск по людям</title>
     <meta charset="utf-8">
+
+    <script>
+        function search() {
+            alert(document.getElementById("letters").value);
+        }
+    </script>
+
 </head>
 
 <body>
     <h1>поиск по людям</h1>
+    Введите первые буквы фамилии человека:
+    <input type="text" id="letters" on/>
 
 </body>
 </html>
