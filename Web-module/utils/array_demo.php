c8733e7d15dbb55b0273d4014b74530e549a556e
Tema <tema@192.168.1.3>
2022-07-03 17:44:31 +0300

    array demo 6

--- Web-module/utils/array_demo.php
+++ Web-module/utils/array_demo.php
@@ -1,27 +1,27 @@
 <?php
 
-$numbers = [1, 2, 3, 44, 5];
-foreach($numbers as $x) {
-    echo "$x<br />";
-}
+// $numbers = [1, 2, 3, 44, 5];
+// foreach($numbers as $x) {
+//     echo "$x<br />";
+// }
 
-$people = [
-    ["Yuri", "Moscow"],
-    ["Eugeny", "Kaliningrad"],
-    ["Alexey", "London"],
-    ["Daniil", "Moscow"]
-];
+// $people = [
+//     ["Yuri", "Moscow"],
+//     ["Eugeny", "Kaliningrad"],
+//     ["Alexey", "London"],
+//     ["Daniil", "Moscow"]
+// ];
 
-$i = 1;
-foreach($people as $person) {
-    $name = $person[0];
-    $city = $person[1];
-    echo("$i. $name from $city <br />");
-    $i = $i + 1;
-}
+// $i = 1;
+// foreach($people as $person) {
+//     $name = $person[0];
+//     $city = $person[1];
+//     echo("$i. $name from $city <br />");
+//     $i = $i + 1;
+// }
 
-$person1 = array("FirstName" => "Yuri", "LastName" => "Andrienko", "City" => "Moscow", "Salary" => 123456);
-echo $person1['FirstName']." ".$person1['LastName']." ".$person1['City']." ".$person1['Salary'];
+// $person1 = array("FirstName" => "Yuri", "LastName" => "Andrienko", "City" => "Moscow", "Salary" => 123456);
+// echo $person1['FirstName']." ".$person1['LastName']." ".$person1['City']." ".$person1['Salary'];
 
 $people2 [
     array("FirstName" => "Yuri", "LastName" => "Andrienko", "City" => "Moscow", "Salary" => 123456),
@@ -32,3 +32,5 @@ $people2 [
 foreach($people2 as $person2) {
     echo $person2['FirstName']." ".$person2['LastName']." ".$person2['City']." ".$person2['Salary'];
 }
+
+?>
