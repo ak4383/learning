ba7eccb0d0afb7e014cdecd0415ee2295c9526dd
Tema <tema@192.168.1.3>
2022-07-03 17:46:56 +0300

    array demo 7

--- Web-module/utils/array_demo.php
+++ Web-module/utils/array_demo.php
@@ -1,29 +1,6 @@
 <?php
 
-// $numbers = [1, 2, 3, 44, 5];
-// foreach($numbers as $x) {
-//     echo "$x<br />";
-// }
-
-// $people = [
-//     ["Yuri", "Moscow"],
-//     ["Eugeny", "Kaliningrad"],
-//     ["Alexey", "London"],
-//     ["Daniil", "Moscow"]
-// ];
-
-// $i = 1;
-// foreach($people as $person) {
-//     $name = $person[0];
-//     $city = $person[1];
-//     echo("$i. $name from $city <br />");
-//     $i = $i + 1;
-// }
-
-// $person1 = array("FirstName" => "Yuri", "LastName" => "Andrienko", "City" => "Moscow", "Salary" => 123456);
-// echo $person1['FirstName']." ".$person1['LastName']." ".$person1['City']." ".$person1['Salary'];
-
-$people2 [
+$people2 = [
     array("FirstName" => "Yuri", "LastName" => "Andrienko", "City" => "Moscow", "Salary" => 123456),
     array("FirstName" => "Vasya", "LastName" => "Pupkin", "City" => "Moscow", "Salary" => 77777),
     array("FirstName" => "Masha", "LastName" => "Mashkina", "City" => "London", "Salary" => 300000)
