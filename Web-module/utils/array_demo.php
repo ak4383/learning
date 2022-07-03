dcc98a85b30ade5ad249d637691e976771d833ea
Tema <tema@192.168.1.3>
2022-07-03 17:40:13 +0300

    array demo 4

--- Web-module/utils/array_demo.php
+++ Web-module/utils/array_demo.php
@@ -23,12 +23,12 @@ foreach($people as $person) {
 $person1 = array("FirstName" => "Yuri", "LastName" => "Andrienko", "City" => "Moscow", "Salary" => 123456);
 echo $person1['FirstName']." ".$person1['LastName']." ".$person1['City']." ".$person1['Salary'];
 
-$people [
+$people2 [
     array("FirstName" => "Yuri", "LastName" => "Andrienko", "City" => "Moscow", "Salary" => 123456),
     array("FirstName" => "Vasya", "LastName" => "Pupkin", "City" => "Moscow", "Salary" => 77777),
     array("FirstName" => "Masha", "LastName" => "Mashkina", "City" => "London", "Salary" => 300000),
 ];
 
-foreach($people as $person) {
-    echo $person['FirstName']." ".$person1['LastName']." ".$person1['City']." ".$person1['Salary'];
+foreach($people2 as $person2) {
+    echo $person2['FirstName']." ".$person2['LastName']." ".$person2['City']." ".$person2['Salary'];
 }
